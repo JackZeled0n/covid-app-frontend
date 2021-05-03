d6100273d9f767f8370727d2cbf0b697102217df
JackZeled0n <jackiezeledon@gmail.com>
2021-05-03 14:31:21 -0600

    Add login frontend design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/authentication/login/login.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 const routes: Routes = [
     {
         path: ''
+        , component: LoginComponent
+    },
+    {
+        path: 'dasboard'
         , component: DashboardComponent
     }
 ];
