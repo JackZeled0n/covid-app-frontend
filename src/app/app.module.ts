904a41ac0d853f7e588da46c4799cb9d80a01989
JackZeled0n <jackiezeledon@gmail.com>
2021-05-03 11:45:16 -0600

    Add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './modules/authentication/login/login.component';
@@ -13,6 +14,7 @@ import { DashboardComponent } from './modules/dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule
+    , AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
