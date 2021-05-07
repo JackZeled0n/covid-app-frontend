9d5d50644e6d230b48ad54e3c8fde764691d90dc
JackZeled0n <jackiezeledon@gmail.com>
2021-05-07 17:53:44 -0600

    Incorporate login functionality^C

--- src/app/modules/authentication/login/login.component.ts
+++ src/app/modules/authentication/login/login.component.ts
@@ -1,17 +1,42 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import * as AOS from 'aos';
+import { AuthServiceService } from 'src/app/core/services/auth-service.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthServiceService) {}
 
   ngOnInit(): void {
     AOS.init();
+    this.initForm();
   }
 
+  initForm(): void {
+    this.formGroup = new FormGroup({
+      userName: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  login(): void {
+    if (this.formGroup.valid) {
+      this.authService.login(this.formGroup.value).subscribe(
+        (result) => {
+          console.log(result);
+          alert(result);
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error.error.message);
+        }
+      );
+    }
+  }
 }
