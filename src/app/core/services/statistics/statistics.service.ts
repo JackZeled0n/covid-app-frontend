4de058bc597827a7a0dcc5764f90bfbf9de7e9ae
JackZeled0n <jackiezeledon@gmail.com>
2021-05-09 22:48:41 -0600

    Add service getByContinentName & updateCases

--- src/app/core/services/statistics/statistics.service.ts
+++ src/app/core/services/statistics/statistics.service.ts
@@ -1,15 +1,39 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-
+import { Statistics } from '../../interfaces/statistics';
 @Injectable({
   providedIn: 'root',
 })
 export class StatisticsService {
   constructor(private http: HttpClient) {}
 
-  getStatistics(): Observable<any> {
-    return this.http.get(environment.developmentUrl + 'sync');
+  protected headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
+
+  getStatistics(): Observable<Statistics[]> {
+    return this.http.get<Statistics[]>(
+      environment.developmentUrl + 'statistics'
+    );
+  }
+
+  getByContinentName(continentName: string): Observable<Statistics[]> {
+    return this.http.get<Statistics[]>(
+      environment.developmentUrl +
+        `statistics/by-continent-name?continentName=${continentName}`
+    );
+  }
+
+  updateCases(body: any): Observable<any> {
+    const data = JSON.stringify(body);
+    return this.http.put<Statistics>(
+      environment.developmentUrl + 'statistics/new-cases',
+      data,
+      {
+        headers: this.headers,
+      }
+    );
   }
 }
